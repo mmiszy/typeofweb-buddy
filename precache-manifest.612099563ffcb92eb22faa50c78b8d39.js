2e49349c2958d9063e700657483a4053ae29972b
Michal Miszczyszyn <michal@mmiszy.pl>
2019-11-13 14:40:59 +0000

    Updates

---
+++ precache-manifest.612099563ffcb92eb22faa50c78b8d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21416cc00bf9f25ace33537e86b9017",
+    "url": "/typeofweb-buddy/index.html"
+  },
+  {
+    "revision": "c954d09124d49d703857",
+    "url": "/typeofweb-buddy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c4ac7e4090135ea57bd0",
+    "url": "/typeofweb-buddy/static/js/2.e26835c8.chunk.js"
+  },
+  {
+    "revision": "c954d09124d49d703857",
+    "url": "/typeofweb-buddy/static/js/main.1093a75f.chunk.js"
+  },
+  {
+    "revision": "8658ca248195f973b5df",
+    "url": "/typeofweb-buddy/static/js/runtime-main.fe31ac1c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/typeofweb-buddy/static/media/logo.25bf045c.svg"
+  }
+]);
